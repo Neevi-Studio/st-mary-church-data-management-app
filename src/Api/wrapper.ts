18215ea8ad1cd456dc34203852bea9b0815217e4
botross <botrosazizbotros@gmail.com>
2024-07-23 14:51:56 +0700

    chore: Update API base path to use new server URL
    This commit updates the API base path in the `wrapper.ts` file to use the new server URL `http://54.84.175.33/api`. The previous base path `http://3.135.166.30:3333` is commented out. This change ensures that the API requests are made to the correct server.

--- src/Api/wrapper.ts
+++ src/Api/wrapper.ts
@@ -14,13 +14,15 @@ globalAxios.interceptors.request.use((config) => {
 
 const ABORT_CONTROLLER = new AbortController();
 export const AXIOS_CONFIG = new Configuration({
-  basePath: 'http://3.135.166.30:3333',
+  basePath: 'http://54.84.175.33/api',
+  // basePath: 'http://3.135.166.30:3333',
   // basePath: process.env.NEXT_PUBLIC_API_URL,
   baseOptions: {
     signal: ABORT_CONTROLLER.signal,
     // MONKEY PATCH
     timeout: 100000,
-    basePath: 'http://3.135.166.30:3333',
+    basePath: 'http://54.84.175.33/api',
+    // basePath: 'http://3.135.166.30:3333',
     // baseURL: process.env.NEXT_PUBLIC_API_URL
   }
 });
